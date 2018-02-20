52d1b94e4019edd0f38643e1bfb7517899b6ff47
gwaerondor <gwaerondor@gmail.com>
2018-02-20 17:15:37 +0100

    Added skeleton for solving Day 10 (2)

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,27 +1,31 @@
 module Day10 where
+import Data.Char (ord)
 
 type Step = Int
 type Skip = Int
 type Length = Int
 
-day10_1 :: ([Int], Int)
+day10_1 :: Int
 day10_1 = product $ take 2 $ hash [0..255] ils
   where
-    ils = [70, 66, 255, 2, 48, 0, 54, 48, 80, 141, 244, 254, 160, 108, 1, 41]
+    ils = [70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41]
 
-hash inputList inputLengths = hash' inputList inputLengths skips shifts
+--------------------------------------------------------------------------------
+hash inputList inputLengths = hash' inputList inputLengths skips shifts rounds inputLengths
   where
     skips = 0
     shifts = 0
+    rounds = 1
 
-hash' :: [Int] -> [Length] -> Skip -> Int -> [Int]
-hash' xs [] _ shifts = unshift shifts xs
-hash' xs (il:ils) s shifts = hash' newXs ils newSkip newShifts
+hash' :: [Int] -> [Length] -> Skip -> Int -> Int -> [Length] -> [Int]
+hash' xs [] _ shifts 1 _ = unshift shifts xs
+hash' xs [] skips shifts rounds initialInput = hash' xs initialInput skips shifts (rounds - 1) initialInput
+hash' xs (il:ils) skips shifts rounds initialInput = hash' newXs ils newSkip newShifts rounds initialInput
   where
     tied = reverseFirst il xs
-    newXs = shift (il + s) tied
-    newSkip = s + 1
-    newShifts = shifts + il + s
+    newXs = shift (il + skips) tied
+    newSkip = skips + 1
+    newShifts = shifts + il + skips
 
 tie :: [a] -> Length -> [a]
 tie xs length = reverseFirst length xs
@@ -37,3 +41,21 @@ shift steps xs = (drop n xs) ++ (take n xs)
 unshift :: Int -> [a] -> [a]
 unshift 0 xs = xs
 unshift steps xs = unshift (steps - 1) ((last xs):(init xs))
+
+--------------------------------------------------------------------------------
+day10_2 :: String
+day10_2 = toHex $ hash2 [0..255] ils
+  where
+    ils = (map ord "70,66,255,2,48,0,54,48,80,141,244,254,160,108,1,41") ++ [17, 31, 73, 47, 23]
+
+hash2 inputList inputLengths = denseHash $ hash' inputList inputLengths skips shifts rounds inputLengths
+  where
+    skips = 0
+    shifts = 0
+    rounds = 64
+
+denseHash :: [Int] -> [Int]
+denseHash xs = error "not finished!"
+
+toHex :: [Int] -> String
+toHex _ = error "not finished!"
